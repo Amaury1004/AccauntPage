baf7d2b03993339edc0371f6377a24d5cee5d3f6
Amaury <asasincer@gmail.com>
2025-01-23 07:16:13 +0200

    Initial Commit

---
+++ AccauntPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccauntPage
+//
+//  Created by MakOSim on 23.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
