f72c3441e2658760cd46e555af9baa553a39c3ed
Amaury <asasincer@gmail.com>
2025-01-25 02:28:21 +0200

    готова

--- AccauntPage/Profil.swift
+++ AccauntPage/Profil.swift
@@ -6,20 +6,21 @@ class Profil: UIViewController {
     var blackMen = UIImageView()
     let nameProfil = UILabel()
     let descriptionProfil = UILabel()
-    let icon = UIImageView()
     //let icon2 = ImageViewInsets(insets: UIEdgeInsets(top: 5, left: 4, bottom: 5, right: 4))
     let memeLable = UILabel()
     let memesStack = UIStackView()
-    let lightLabel = UILabel()
+    let swithLable = SwitchLight(frame: CGRect(x: 0, y: 0, width: 388, height: 44))
     let lightSwitch = UISwitch()
+    
     
     //let scrollView = UIScrollView(frame: .zero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
         setupUI()
-        setupInitialTheme()
+        
 
     }
     
@@ -51,28 +52,12 @@ class Profil: UIViewController {
         descriptionProfil.translatesAutoresizingMaskIntoConstraints = false
     
         
-        icon.image = UIImage(systemName: "lamp.ceiling.fill")
-        icon.clipsToBounds = true
-        icon.layer.cornerRadius = 7
-        icon.translatesAutoresizingMaskIntoConstraints = false
-        icon.backgroundColor = UIColor(red: 0, green: 122/255, blue: 1, alpha: 1)
-        
-        
         memeLable.text = "Відбірні меми Чоткого"
         memeLable.textColor = .black
         memeLable.font = UIFont.boldSystemFont(ofSize: 20)
         memeLable.textAlignment = .center
         memeLable.translatesAutoresizingMaskIntoConstraints = false
-        
-        lightLabel.text = "Выключить свет"
-        lightLabel.font = UIFont.systemFont(ofSize: 14)
-        lightLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        lightSwitch.onTintColor = .gray
-        lightSwitch.addTarget(self, action: #selector(switchTheme), for: .valueChanged)
-        lightSwitch.translatesAutoresizingMaskIntoConstraints = false
-        
-
+    
         let memes = ["meme1", "meme2", "meme3", "meme4","meme5", "meme4"]
         
         let firstRow = UIStackView()
@@ -85,15 +70,14 @@ class Profil: UIViewController {
         secondRow.spacing = 1
         secondRow.distribution = .fillEqually
 
-        // Добавляем картинки в первый ряд
         for i in 0..<3 {
             let imageView = UIImageView(image: UIImage(named: memes[i]))
-            imageView.contentMode = .scaleAspectFill // Лучше чем .scaleAspectFit для сетки
-            imageView.clipsToBounds = true // Обрезает лишнее, если нужно
+            imageView.contentMode = .scaleAspectFill
+            imageView.clipsToBounds = true
             firstRow.addArrangedSubview(imageView)
         }
+        
 
-        // Добавляем картинки во второй ряд
         for i in 3..<6 {
             let imageView = UIImageView(image: UIImage(named: memes[i]))
             imageView.contentMode = .scaleAspectFill
@@ -110,23 +94,26 @@ class Profil: UIViewController {
         memesStack.translatesAutoresizingMaskIntoConstraints = false
         
         
+        swithLable.translatesAutoresizingMaskIntoConstraints = false
+        lightSwitch.addTarget(self, action: #selector(switchTheme(_:)), for: .valueChanged);        lightSwitch.translatesAutoresizingMaskIntoConstraints = false
+        
+        
             
         //view.addSubview(scrollView)
         view.addSubview(nameProfil)
         view.addSubview(blackMen)
         view.addSubview(descriptionProfil)
-        view.addSubview(icon)
         view.addSubview(memeLable)
+        view.addSubview(swithLable)
         view.addSubview(lightSwitch)
-        view.addSubview(lightLabel)
+        
+        
         view.addSubview(memesStack)
         
+        
+        
         setupConstraints()
-    }
     
-    func setupInitialTheme() {
-        let darkMode = lightSwitch.isOn
-        updateUI(for: darkMode)
     }
     
     func setupConstraints() {
@@ -160,37 +147,33 @@ class Profil: UIViewController {
             memesStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
             memesStack.topAnchor.constraint(equalTo: memeLable.bottomAnchor, constant:  30),
             
-            icon.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -35),
-            icon.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            icon.widthAnchor.constraint(equalToConstant: 30),
-            icon.heightAnchor.constraint(equalToConstant: 30),
+            swithLable.topAnchor.constraint(equalTo: memesStack.bottomAnchor, constant: 42),
             
-            lightLabel.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 16),
-            lightLabel.centerYAnchor.constraint(equalTo: icon.centerYAnchor),
-            lightLabel.heightAnchor.constraint(equalToConstant: 44),
-            
-            lightSwitch.leadingAnchor.constraint(equalTo: lightLabel.trailingAnchor, constant: 8),
+            lightSwitch.centerYAnchor.constraint(equalTo: swithLable.centerYAnchor, constant: 22),
             lightSwitch.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            lightSwitch.centerYAnchor.constraint(equalTo: icon.centerYAnchor),
-            lightSwitch.heightAnchor.constraint(equalToConstant: 44)
-    
+            
         ])
     }
     
     @objc func switchTheme(_ sender: UISwitch) {
-        let darkMode = sender.isOn
-        updateUI(for: darkMode)
+    let darkMode = sender.isOn
+    updateUI(for: darkMode)
     }
-    
-    
     
     func updateUI(for darkMode: Bool) {
         view.backgroundColor = darkMode ? .black : .white
         nameProfil.textColor = darkMode ? .white : .black
         descriptionProfil.textColor = darkMode ? .white : .black
         memeLable.textColor = darkMode ? .white : .black
-        icon.tintColor = darkMode ? .white : .black
-        lightLabel.textColor = darkMode ? .white : .black
+        swithLable.switchLabel.textColor = darkMode ? .white : .black
     }
-}
+
+
+    
+    
+
+    }
+
+
+    
 
