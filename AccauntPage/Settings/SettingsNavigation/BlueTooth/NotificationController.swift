1233e4f43e764400fe400051a6fc23913d233687
Amaury <asasincer@gmail.com>
2025-03-20 17:04:54 +0200

    Fix

---
+++ AccauntPage/Settings/SettingsNavigation/BlueTooth/NotificationController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class NotificationsSettingsController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        title = "Notifications"
+        
+        navigationItem.backButtonTitle = "Settings"
+    }
+}
