c2fdad600287dd103257df137f42ecb90cf28c73
Amaury <asasincer@gmail.com>
2025-04-04 13:48:14 +0300

    Добавил в Cellular оброботку смешной кнопочки

--- AccauntPage/Settings/SettingsNavigation/CellularController/CellularController.swift
+++ AccauntPage/Settings/SettingsNavigation/CellularController/CellularController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SafariServices
 
 protocol CellularSettingsDelegate: AnyObject {
     func didUpdateSetting(value: String, for key: String)
@@ -11,33 +12,102 @@ class CellularSettingsController: UIViewController, CellularSettingsDelegate {
     
     let content = SetupCell.setupCellularGroup()
     var model: CellularSettingsData?
+    let cellularView = UIView()
+    let mainTitle = UILabel()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Cellular"
         view.backgroundColor = .white
-        
         navigationItem.backButtonTitle = "Settings"
-        
+
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(CellularCell.self, forCellReuseIdentifier: "CellularCell")
-        tableView.allowsSelection = false 
 
-        
         view.addSubview(tableView)
         tableView.translatesAutoresizingMaskIntoConstraints = false
+
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+
+        setupTableHeader()
+    }
+    
+    func setupTableHeader() {
+        let headerView = UIView()
+        headerView.backgroundColor = .white
+
+        let icon = InsetImageView()
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.setup(image: UIImage(systemName: "antenna.radiowaves.left.and.right")!.withRenderingMode(.alwaysTemplate),
+                   insets: UIEdgeInsets(top: 10, left: 8, bottom: 10, right: 8))
+        icon.tintColor = .white
+        icon.contentMode = .scaleAspectFit
+        icon.backgroundColor = .green
+        icon.layer.cornerRadius = 17
+        icon.clipsToBounds = true
+
+        let mainTitle = UILabel()
+        mainTitle.numberOfLines = 0
+        mainTitle.font = .systemFont(ofSize: 14)
+        mainTitle.translatesAutoresizingMaskIntoConstraints = false
+        mainTitle.textAlignment = .center
+        mainTitle.isUserInteractionEnabled = true
+
+        let fullText = "CELLULAR \n\nFind out how much data you're using, set data restrictions, and manage carrier settings such as eSIM and Wi-Fi calling. "
+        let linkText = "Learn more..."
+
+        let attributedText = NSMutableAttributedString(string: fullText + linkText)
+
+        let linkRange = NSRange(location: fullText.count, length: linkText.count)
+        attributedText.addAttribute(.foregroundColor, value: UIColor.systemBlue, range: linkRange)
+        attributedText.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: linkRange)
+
+        mainTitle.attributedText = attributedText
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(learnMoreTapped(_:)))
+        mainTitle.addGestureRecognizer(tapGesture)
+
+        headerView.addSubview(icon)
+        headerView.addSubview(mainTitle)
+
+        NSLayoutConstraint.activate([
+            icon.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 16),
+            icon.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            icon.widthAnchor.constraint(equalToConstant: 60),
+            icon.heightAnchor.constraint(equalToConstant: 60),
+
+            mainTitle.topAnchor.constraint(equalTo: icon.bottomAnchor, constant: 8),
+            mainTitle.leadingAnchor.constraint(equalTo: headerView.leadingAnchor, constant: 20),
+            mainTitle.trailingAnchor.constraint(equalTo: headerView.trailingAnchor, constant: -20),
+            mainTitle.bottomAnchor.constraint(equalTo: headerView.bottomAnchor, constant: -16)
+        ])
+
+        headerView.setNeedsLayout()
+        headerView.layoutIfNeeded()
+
+        let targetSize = CGSize(width: view.bounds.width, height: UIView.layoutFittingCompressedSize.height)
+        let height = headerView.systemLayoutSizeFitting(targetSize).height + 50
+        headerView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: height)
+
+        tableView.tableHeaderView = headerView
     }
     
     @objc func cellularDataSwitchChanged(_ sender: UISwitch) {
         UserDefaults.standard.set(sender.isOn, forKey: "CellularDataEnabled")
         tableView.reloadData()
+    }
+    @objc func learnMoreTapped(_ gesture: UITapGestureRecognizer) {
+        guard let url = URL(string: "https://www.google.com") else { return }
+        let safariVC = SFSafariViewController(url: url)
+        safariVC.preferredControlTintColor = .systemBlue 
+        present(safariVC, animated: true, completion: nil)
     }
     
     func didUpdateSetting(value: String, for key: String) {
@@ -83,6 +153,7 @@ extension CellularSettingsController: UITableViewDataSource {
 
 extension CellularSettingsController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
